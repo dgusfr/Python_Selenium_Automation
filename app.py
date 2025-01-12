ad97657f4021fee7dda45ef17c505330bdefc3f1
dgusfr <diegogustavo.fr@gmail.com>
2025-01-11 21:20:29 -0300

    fix(): update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'       
-app.config['MYSQL_PASSWORD'] = ''       # Substitua pela sua senha MySQL
+app.config['MYSQL_PASSWORD'] = ''       
 app.config['MYSQL_DB'] = 'cadastro_db'
 
 mysql = MySQL(app)
